b867f155e1ac62e44345a77d673ddd341f7bb359
Arnav Dugar <arnav@counsyl.com>
2015-06-23 10:16:12 -0700

    Fixed Formatting.

--- go.jsx
+++ go.jsx
@@ -1,6 +1,6 @@
 board_width = 700;
 board_height = 700;
-grid_size = 19;
+grid_size = 8;
 
 var Board = React.createClass({
     getInitialState: function() {
@@ -22,24 +22,37 @@ var Board = React.createClass({
                 } else if(this.state[index] == 2) {
                     var color = "black";
                 }
-                var piece = <Piece key={"p" + index} x={x} y={y} color={color}/>;
-                pieces.push(piece);
+                pieces.push(<Piece key={"p" + index} x={x} y={y} color={color}
+                    onClickHandler={this.onPieceClick}/>);
             }
         }
         var lines = [];
         for(var i = 0; i < grid_size; i++) {
             var y = board_width * (i + 0.5) / grid_size;
-            lines.push(<line key={"y" + i} className="grid" x1="0" x2={board_width} y1={y} y2={y}/>);
+            var x_start = 0.5 * board_width/grid_size;
+            var x_end = board_width * (1 - 0.5 / grid_size);
+            lines.push(<line key={"y" + i} className="grid" x1={x_start} x2={x_end} y1={y} y2={y}/>);
         }
         for(var i = 0; i < grid_size; i++) {
             var x = board_height * (i + 0.5) / grid_size;
-            lines.push(<line key={"x" + i} className="grid" x1={x} x2={x} y1="0" y2={board_height}/>);
+            var y_start = 0.5 * board_height/grid_size;
+            var y_end = board_height * (1 - 0.5 / grid_size);
+            lines.push(<line key={"x" + i} className="grid" x1={x} x2={x} y1={y_start} y2={y_end}/>);
         }
         return (
             <svg className="board" height={board_width} width={board_height}>
                 {lines}{pieces}
             </svg>
         );
+    },
+    onPieceClick: function(x, y) {
+        console.log(x, y)
+        var index = y * grid_size + x;
+        this.setState(function(previousState, currentProps) {
+            var obj = {};
+            obj[index] = (previousState[index] + 1) % 3;
+            return obj;
+        });
     }
 });
 
@@ -47,16 +60,11 @@ var Piece = React.createClass({
     render: function() {
         var x = board_width * (this.props.x + 0.5) / grid_size;
         var y = board_width * (this.props.y + 0.5) / grid_size;
-        return (<circle className={"piece " + this.props.color} cx={x} cy={y} onClick={this.onClick}></circle>);
-    },
-    onClick: function(event) {
-        clickHandler(event, this);
+        var r = Math.round(0.9 * board_width / (2 * grid_size + 2));
+        return (<circle className={"piece " + this.props.color} cx={x} cy={y} r={r}
+            onClick={this.props.onClickHandler.bind(null, this.props.x, this.props.y)}></circle>);
     }
 });
 
 React.render(<Board/>, document.getElementById('content'));
-
-function clickHandler(event, target) {
-    console.log("Clicked x = " + target.props.x + ", y = " + target.props.y + ".");
-}
 
